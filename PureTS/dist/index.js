be7ce9374682acfd985ce84915ef67681a4c7ec8
Ojiamboloc <ojiamboloc@gmail.com>
2023-01-07 00:12:55 -0800

    getters and setters

--- PureTS/dist/index.js
+++ PureTS/dist/index.js
@@ -1,10 +1,35 @@
 "use strict";
-class User {
-    constructor(email, name) {
-        this.city = "Nairobi";
-        this.email = email;
-        this.name = name;
+/*class User {
+    email:string;
+    name:string;
+    private readonly city:string="Nairobi"
+    constructor (email:string,name:string){
+        this.email=email;
+        this.name=name;
     }
 }
-const felix = new User("ojiamboloc@gmail.com", "Ojiambo Nambasi");
+const felix=new User("ojiamboloc@gmail.com","Ojiambo Nambasi")*/
 //felix.city="Nairobi"
+class User {
+    constructor(email, name, userId) {
+        this.email = email;
+        this.name = name;
+        this.userId = userId;
+        this._courseCount = 1;
+        this.city = "Nairobi";
+    }
+    deleteToken() {
+    }
+    get getAppleEmail() {
+        return `apple${this.email}`;
+    }
+    get courseCount() {
+        return this._courseCount;
+    }
+    set courseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error("Course count should be more than 1");
+        }
+        this._courseCount = courseNum;
+    }
+}
--- PureTS/src/index.ts
+++ PureTS/src/index.ts
@@ -1,11 +1,36 @@
-class User {
+/*class User {
     email:string;
     name:string;
-   readonly city:string="Nairobi"
+    private readonly city:string="Nairobi"
     constructor (email:string,name:string){
         this.email=email;
         this.name=name;
     }
 }
-const felix=new User("ojiamboloc@gmail.com","Ojiambo Nambasi")
+const felix=new User("ojiamboloc@gmail.com","Ojiambo Nambasi")*/
 //felix.city="Nairobi"
+class User {
+    private _courseCount=1
+    private readonly city:string="Nairobi"
+    constructor (
+        public email:string,
+        public name:string,
+        private userId:string)
+        {
+     
+    }
+    private deleteToken(){
+
+    }
+    get getAppleEmail():string{
+return `apple${this.email}`
+    }
+
+get courseCount():number{
+    return this._courseCount
+}set courseCount(courseNum){
+ if(courseNum<=1){
+    throw new Error("Course count should be more than 1")
+ }this._courseCount=courseNum
+}
+}
