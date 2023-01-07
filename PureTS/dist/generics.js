814577924ebdd6e5c82c55b59b4bfe528dc13938
Ojiamboloc <ojiamboloc@gmail.com>
2023-01-07 00:56:50 -0800

    generics

---
+++ PureTS/dist/generics.js
@@ -0,0 +1,17 @@
+"use strict";
+//const score:Array<number>=[]
+//const names:Array<string>=[]
+function identityOne(val) {
+    return val;
+}
+function identityTwo(val) {
+    return val;
+}
+function identityThree(val) {
+    return val;
+}
+//identityThree(true)
+function identityFour(val) {
+    return val;
+}
+identityFour({});
