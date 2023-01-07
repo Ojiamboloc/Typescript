39176a6f04ec68cb630f3e38ef3c50006c2a6b4d
Ojiamboloc <ojiamboloc@gmail.com>
2023-01-07 03:13:11 -0800

    discriminatory narrowing

--- PureTS/dist/detection.js
+++ PureTS/dist/detection.js
@@ -40,3 +40,22 @@ function getFood(pet) {
         return "bird food";
     }
 }
+function getTrueShape(shape) {
+    if (shape.kind === "circle") {
+        return Math.PI * shape.radius ** 2;
+    }
+    //return shape.side * shape.side;
+}
+function getArea(shape) {
+    switch (shape.kind) {
+        case "circle":
+            return Math.PI * shape.radius ** 2;
+        case "square":
+            return shape.side * shape.side;
+        case "rectangle":
+            return shape.length * shape.width;
+        default:
+            const _defaultforShape = shape;
+            return _defaultforShape;
+    }
+}
--- PureTS/src/detection.ts
+++ PureTS/src/detection.ts
@@ -11,40 +11,73 @@ function providId(id: string | null) {
   }
   id.toLocaleLowerCase();
 }
-interface User{
-    name:string,
-    email:string
+interface User {
+  name: string;
+  email: string;
 }
-interface Admin{
-    name:string,
-    email:string,
-    isAdmin:boolean
+interface Admin {
+  name: string;
+  email: string;
+  isAdmin: boolean;
 }
 //in narrowing
-function isAdminAccount(account:User | Admin){
-    if ("isAdmin"in account){
-        return account.isAdmin
-    }
+function isAdminAccount(account: User | Admin) {
+  if ("isAdmin" in account) {
+    return account.isAdmin;
+  }
 }
 //instance of narrowing
-function logValue(x:Date | String){
-    if(x instanceof Date){
-        console.log(x.toUTCString());
-    }else{
-        console.log(x.toUpperCase());
-    }
+function logValue(x: Date | String) {
+  if (x instanceof Date) {
+    console.log(x.toUTCString());
+  } else {
+    console.log(x.toUpperCase());
+  }
 }
-type Fish={swim:()=>void};
-type Bird={fly:()=>void};
-function isFish(pet:Fish |Bird):pet is Fish{
-    return (pet as Fish).swim !==undefined
+type Fish = { swim: () => void };
+type Bird = { fly: () => void };
+function isFish(pet: Fish | Bird): pet is Fish {
+  return (pet as Fish).swim !== undefined;
 }
-function getFood(pet:Fish | Bird){
-    if(isFish(pet)){
-       pet
-       return "fish food"
-    }else{
-        pet
-        return "bird food"
-    }
+function getFood(pet: Fish | Bird) {
+  if (isFish(pet)) {
+    pet;
+    return "fish food";
+  } else {
+    pet;
+    return "bird food";
+  }
 }
+//discriminated unions
+interface Circle {
+  kind: "circle";
+  radius: number;
+}
+interface Square {
+  kind: "square";
+  side: number;
+}
+interface Rectangle {
+  kind: "rectangle";
+  length: number;
+  width: number;
+}
+type Shape = Circle | Square |Rectangle;
+function getTrueShape(shape: Shape) {
+  if (shape.kind === "circle") {
+    return Math.PI * shape.radius ** 2;
+  }
+  //return shape.side * shape.side;
+}
+function getArea(shape: Shape) {
+  switch (shape.kind) {
+    case "circle":
+      return Math.PI * shape.radius ** 2;
+    case "square":
+      return shape.side * shape.side;
+      case "rectangle":
+        return shape.length*shape.width
+      default:
+        const _defaultforShape:never=shape
+        return _defaultforShape
+}}
