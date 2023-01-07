897873cfa42bfc113abedb710507fa395e92dd31
Ojiamboloc <ojiamboloc@gmail.com>
2023-01-07 02:43:28 -0800

    instance narrowing

--- PureTS/dist/detection.js
+++ PureTS/dist/detection.js
@@ -12,8 +12,31 @@ function providId(id) {
     }
     id.toLocaleLowerCase();
 }
+//in narrowing
 function isAdminAccount(account) {
     if ("isAdmin" in account) {
         return account.isAdmin;
+    }
+}
+//instance of narrowing
+function logValue(x) {
+    if (x instanceof Date) {
+        console.log(x.toUTCString());
+    }
+    else {
+        console.log(x.toUpperCase());
+    }
+}
+function isFish(pet) {
+    return pet.swim !== undefined;
+}
+function getFood(pet) {
+    if (isFish(pet)) {
+        pet;
+        return "fish food";
+    }
+    else {
+        pet;
+        return "bird food";
     }
 }
--- PureTS/src/detection.ts
+++ PureTS/src/detection.ts
@@ -20,8 +20,31 @@ interface Admin{
     email:string,
     isAdmin:boolean
 }
+//in narrowing
 function isAdminAccount(account:User | Admin){
     if ("isAdmin"in account){
         return account.isAdmin
+    }
+}
+//instance of narrowing
+function logValue(x:Date | String){
+    if(x instanceof Date){
+        console.log(x.toUTCString());
+    }else{
+        console.log(x.toUpperCase());
+    }
+}
+type Fish={swim:()=>void};
+type Bird={fly:()=>void};
+function isFish(pet:Fish |Bird):pet is Fish{
+    return (pet as Fish).swim !==undefined
+}
+function getFood(pet:Fish | Bird){
+    if(isFish(pet)){
+       pet
+       return "fish food"
+    }else{
+        pet
+        return "bird food"
     }
 }
